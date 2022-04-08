6d7669e2cfe26c1e33f9cbb0af7ca3be6bab0c72
cardosorenanalves <renancardoso0307@gmail.com>
2022-04-08 14:40:27 -0300

    Estudo de Estados

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,13 @@
+import './style.css'
+
+export function Card(props){
+    return(
+        <div className='card'>
+           <strong>{props.name}</strong> 
+            <small>{props.time}</small>
+ 
+        </div>
+
+
+    )
+}
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,23 @@
-import './style.css'
+import React, { useState} from 'react';
+import './style.css';
+
+import { Card } from '../../components/Card';
+
 export function Home() {
+  const[studentName, setStudentName] = useState("Renan");
+
   return (
     <div className='container'>
-      <h1>Lista de Presença</h1>
-      <input type="text" placeholder="Digite o nome..." id="" />
+      <h1>Nome: {studentName}</h1>
+
+      <input type="text"
+       placeholder="Digite o nome..." 
+      onChange={e => setStudentName(e.target.value) }/>
+
       <button type="button">Adicionar</button>
+
+      <Card name="Rodrigo Alves Cardoso" time="15:23:02"/>
+      <Card name="Renan Alves Cardoso" time="12:05:56"/>
     </div>
     )
 }
